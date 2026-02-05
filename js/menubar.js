32c3d63123685e503415b869d9420a77a84c7107
Paarth Bharadwaj <paarth.bharadwaj@student.tugraz.at>
2026-02-05 13:57:38 +0100

    Create structure

--- js/menubar.js
+++
@@ -1,8 +0,0 @@
-// scripts/menu-toggle.js
-const hamburger = document.querySelector('.hamburger');
-const navCenter = document.querySelector('.navbar-center');
-
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navCenter.classList.toggle('open');
-});
